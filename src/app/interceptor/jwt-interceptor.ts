9cf950c8515f07044abfe8a4bf470428eb2614c3
Cesar Abreu <cesarabreu_@outlook.com>
2025-11-05 21:03:42 -0300

    Fix interceptor

--- src/app/interceptor/jwt-interceptor.ts
+++ src/app/interceptor/jwt-interceptor.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '../services/auth-service';
 import { environment } from '../environments/environment';
+import { catchError, throwError } from 'rxjs';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class jwtInterceptor implements HttpInterceptor {
-  constructor(private serviceAuth: AuthService) {}
+  constructor(private serviceAuth: AuthService, private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     let apiReq = req;
@@ -24,6 +27,25 @@ export class jwtInterceptor implements HttpInterceptor {
       });
       return next.handle(clonedReq);
     }
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          console.log('Interceptor 401');
+
+          Swal.fire({
+            icon: 'error',
+            title: 'Sessão Expirada',
+            text: 'Sua sessão expirou. Faça login novamente.',
+            confirmButtonText: 'OK'
+          });
+
+          this.serviceAuth.logout();
+          this.router.navigate(['/login']);
+
+          return throwError(() => new Error('Sessão expirada. Redirecionando para login.'));
+        }
+        return throwError(() => error);
+      })
+    );
   }
 };
